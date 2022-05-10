b435986c318db9bb8c0b6d0ba33b677c2ef684a4
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-05-10 14:06:56 -0400

    Created a cardArray with all of the different images

---
+++ main.js
@@ -0,0 +1,37 @@
+
+const cardArray = [
+  {
+    name: 'fries',
+    img: './images/fries'
+  },
+  {
+    name: 'blank',
+    img: './images/blank'
+  },
+  {
+    name: 'white',
+    img: './images/white'
+  },
+  {
+    name: 'milkshake',
+    img: './images/milkshake'
+  },
+  {
+    name: 'hotdog',
+    img: './images/hotdog'
+  },
+  {
+    name: 'cheeseburger',
+    img: './images/cheeseburger'
+  },
+  {
+    name: 'pizza',
+    img: './images/pizza'
+  },
+  {
+    name: 'icecream',
+    img: './images/ice-cream'
+  },
+]
+
+console.log(cardArray)
